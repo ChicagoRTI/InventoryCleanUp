546c2b4f1cf3c205b5822b78b4f1bc165cafbff6
Lindsay Darling <ledarling@gmail.com>
2019-08-15 09:13:27 -0400

    Almost done except street tree coding

--- Scripts/Davey.R
+++ Scripts/Davey.R
@@ -2,7 +2,7 @@
 # By: Lindsay Darling
 
 # started on: July 9, 2019
-# todo: Script being edited from SESYNC project. Needs lots of work to be applicable, but it's a start
+# todo: Everything is good except the "StreetTree" coding to y/n
 
 # load useful libraries
 install.packages("tidyverse")
@@ -34,54 +34,118 @@ LakeForest <- read_csv(file.path('RawData',
          StreetTree = `LOCATION,C,17`,
          Site = `STREET,C,23`,
          Plot = `SIDE,C,9`,
-         DBH_IN = `DBH,C,4`
+         DBH_in = `DBH,C,4`,
+         HealthWord = `COND,C,9`
          ) %>% 
   
   mutate(DataSource = 'LakeForest',
          InventoryTyp = 'Municipal',
-         DataType = 'Point'
-       ) 
+         DataType = 'Point',
+         LandUse = ''
+       ) %>%
+  mutate(HealthWord=recode(HealthWord,                      
+                          'Critical' = 'Dead',
+                          'Very Good' = 'Excellent')) %>%
+  
+  mutate(Year = lubridate::year(as.Date(`Year`, format = '%m/%d/%Y'))) %>%
+  
+  mutate(SteetTree=recode(StreetTree,
+                          'Street' = 'Y',
+                          'Park/Public Space' = 'N',
+                          'Borderline' = 'N',
+                          'Off ROW' = 'N',
+                          'Unknown' = 'N',
+                          'Woodlot' = 'N')) %>% #Coding street tree y/n FIXME this isn't working
 
-# Remove non-applicable trees
-  LakeForest %>% select(GenusSpecies =! STUMP,
-                        )  
+  filter(GenusSpecies != "stump") %>%
+  filter(GenusSpecies != "Tree Removal No Stump") %>%
+  filter(GenusSpecies != "Tree Removed - Deerpath Golf Course") %>%
+  filter(GenusSpecies != "Tree Removed Not Replaced") %>%
+  filter(GenusSpecies != "vacant site small") %>% 
+  filter(GenusSpecies != "New Tree") %>%
+  filter(GenusSpecies != "New Tree Spring") %>%
+  filter(GenusSpecies != "New Tree Fall") %>% #removing non trees 
+  filter(Latitude != 0 ) # remove a couple of trees without coordinates
+
+
+
+
+#Fix things that won't join
+
+LakeForest$GenusSpecies <- gsub("'", "", LakeForest$GenusSpecies, fixed=TRUE)
+LakeForest$GenusSpecies <- gsub(" x ", " ",LakeForest$GenusSpecies, fixed=TRUE)
+
+LakeForest %<>%
+  mutate(GenusSpecies=recode(GenusSpecies,
+                           'Acer tataricum ginnala' ='Acer ginnala',
+                           'Aesculus Autumn Splendor' ='Aesculus arnoldiana',
+                           'Aesculus carnea'= 'Aesculus carnea',
+                           'Cedrus atlantica' ='Cedrus spp.',
+                           'Crataegus crusgalli' = 'Crataegus crus-galli',
+                           'Gleditsia triacanthos inermis' = 'Gleditsia triacanthos',
+                           'Laburnum watereri' = 'Laburnum alpinum',
+                           'Maackia Amurensis' = 'Maackia amurensis',
+                           'Magnolia soulangiana' = 'Magnolia soulangeana',
+                           'Q. × macdanielli' = 'Quercus macdanielii',
+                           'Quercus macrocarpa robur' = 'Quercus robur',
+                           'Quercus x' = 'Quercus spp.',
+                           'syringa chenensis' = 'Syringa pekinensis',
+                           'Tabebuia umbellata' = 'Unknown spp.',
+                           'Tsuga candensis' = 'Tsuga canadensis',
+                           'Ulmus x' = 'Ulmus hybrid',
+                           'unknown tree' = 'Unknown spp.',
+                           'Ulmus thomasi' = 'Ulmus thomasii',
+                           ))
+
 
 # first read in the species list
-species_list <- read_csv(file.path('Forests_raw',
-                                   'species_reconciliation/species_reconciliation_short.csv'))
+species_list <- read_csv(file.path('RawData',
+                                   'SpeciesList.csv'))
 
-# select columns, rename, and add a genus species column
-species_list %<>% select(`FIA\nCode`, `PLANTS\nCode`, `Common Name`, Genus, Species) %>% 
-  rename(FIA_code = `FIA\nCode`,
-         PLANTS_code =`PLANTS\nCode`, 
-         common_name =`Common Name`, 
-         genus = Genus,
-         species = Species) %>% 
-  mutate(`genus species` = paste0(genus, ' ', species)) %>%
-  rename (genus_species = `genus species`)
 
-# add genus species to FIA_Tree by joining to the species list
-iTree_Baltimore_Tree %<>% left_join(species_list,
-                                    by = c('SPECIES CODE'= 'PLANTS_code'))
+# Join species lists
+LakeForest %<>% left_join(species_list,
+                                    by = c('GenusSpecies'= 'GenusSpecies'))
+
+#Read health lookup
+
+HealthTable <- read.csv(file="C:/Users/Lindsay Darling/Documents/R/InventoryCleanUp/RawData/HealthTable.csv")
+
+#Join health
+
+LakeForest %<>% left_join(HealthTable,
+                          by = c('HealthWord'= 'HealthWord'))
+
 #Final Cleanup
 
-iTree_Baltimore_Tree %<>%
-  select(data_source,
-         plot_ID,
-         sub_plot_ID,
-         tree_ID,
-         obs_year,
-         genus,
-         species,
-         genus_species,
-         dbh_cm,
-         height_m)
+LakeForest %<>%
+  select(DataSource,
+         InventoryTyp,
+         DataType,
+         Latitude,
+         Longitude,
+         LandUse,
+         StreetTree,
+         Year,
+         Site,
+         Plot,
+         TreeID,
+         Genus,
+         Species,
+         GenusSpecies,
+         CommonName,
+         DBH_in,
+         HealthWord,
+         HealthNum)
 
 # double checks
-View(iTree_Baltimore_Tree)
+View(LakeForest)
+View(HealthTable)
+View(species_list)
+
 
 # save out the .csv
-write.csv(iTree_Baltimore_Tree, file = file.path('forests_standardized','Baltimore_iTree_Tree.csv'),
+write.csv(LakeForest, file = file.path('ForestStandardized','LakeForest2019.csv'),
           row.names = FALSE)
 
 # end here
